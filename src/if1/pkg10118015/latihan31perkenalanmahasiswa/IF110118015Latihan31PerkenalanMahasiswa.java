05973b1c0224d2b0f473b9e06730dffc241e95ba
adichandran <55467774+adichandran@users.noreply.github.com>
2019-10-12 16:16:53 +0700

    Add files via upload

---
+++ src/if1/pkg10118015/latihan31perkenalanmahasiswa/IF110118015Latihan31PerkenalanMahasiswa.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118015.latihan31perkenalanmahasiswa;
+
+/**
+ *
+ * @author
+ * NAMA                 : Adi Chandra Nugraha
+ * KELAS                : IF-1
+ * NIM                  : 10118015
+ * Deskripsi Program    : Program ini berisi program menampilkan daftar mahasiswa
+ */
+public class IF110118015Latihan31PerkenalanMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        mahasiswa mahasiswa = new mahasiswa();
+        mahasiswa.perkenalanDiri("10118015","Adi Chandra Nugraha");
+        mahasiswa.perkenalanDiri("10118013","Rifian Joe Premilenio");
+        mahasiswa.perkenalanDiri("10118020","Fiqri Akbar Pratama");
+    }
+    
+}
---
+++ src/if1/pkg10118015/latihan31perkenalanmahasiswa/mahasiswa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118015.latihan31perkenalanmahasiswa;
+
+/**
+ *
+ * @author ACER
+ */
+public class mahasiswa {
+    public String nim;
+    public String nama;
+    
+    public static void perkenalanDiri(String nim,String nama){
+        System.out.println("Hallo Everyone");
+        System.out.println("My NIM is " + nim);
+        System.out.println("My Name is " + nama);
+        System.out.println();
+    }
+}
